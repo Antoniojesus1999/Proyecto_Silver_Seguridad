b41652cc883aa556a516a95efa485e146e14a9b3
Antonio Jesús Ponce Vela <antoniojesuspv99@gmail.com>
2022-12-29 17:54:50 +0100

    implementada bootstrap implementada pantalla de login impelementada rutas implementada auth service y aut-guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './services/auth-guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component: AppComponent,canActivate:[AuthGuard]},
+  {path: 'login', component: LoginComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
