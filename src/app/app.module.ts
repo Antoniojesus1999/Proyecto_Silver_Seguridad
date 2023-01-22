61c006bef1602e377cbdf3baf1665f5bbb14d21e
Antonio Jesús Ponce Vela <antoniojesuspv99@gmail.com>
2023-01-22 21:24:56 +0100

    Modificación sacando componente firma al principal form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormComponent } from './form/form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthGuard } from './services/auth-guard';
-import { FirmaComponent } from './firma/firma.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    FormComponent,
-    FirmaComponent
+    FormComponent
   ],
   imports: [
     ReactiveFormsModule,
